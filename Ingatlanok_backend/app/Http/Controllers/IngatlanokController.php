10ce38564d9503152e5bb6dcebcc06c8b3cff8eb
Bence1102 <szente.bence04@gmail.com>
2025-12-12 10:40:11 +0100

    migrations

---
+++ Ingatlanok_backend/app/Http/Controllers/IngatlanokController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreingatlanokRequest;
+use App\Http\Requests\UpdateingatlanokRequest;
+use App\Models\ingatlanok;
+
+class IngatlanokController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreingatlanokRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ingatlanok $ingatlanok)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ingatlanok $ingatlanok)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateingatlanokRequest $request, ingatlanok $ingatlanok)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ingatlanok $ingatlanok)
+    {
+        //
+    }
+}
